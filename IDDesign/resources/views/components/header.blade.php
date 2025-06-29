f4138240810b65e83eeccda28afa18c78e7c352f
MarekDieska <xdieska@stuba.sk>
2025-06-29 23:32:36 +0200

    start setup

---
+++ IDDesign/resources/views/components/header.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <div class="grid grid-cols-3">
+        <div>
+            <img src="{{ asset('storage/images/logo.webp') }}" alt="logo firmy">
+        </div>
+        <div>
+            <p>ID-design</p>
+        </div>
+    </div>
+</div>
---
+++ IDDesign/resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>ID-design</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+<header class="">
+    @include("../components/header")
+</header>
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+</h1>
+</body>
+</html>
