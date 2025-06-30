580029d13f43357063aacf75d3dac831592081c2
MarekDieska <xdieska@stuba.sk>
2025-06-30 22:12:41 +0200

    landing a about

--- IDDesign/resources/views/components/landing.blade.php
+++ IDDesign/resources/views/components/landing.blade.php
@@ -1,13 +1,17 @@
-<section class="bg-gray-200 h-[80vh] p-2 grid grid-cols-2 items-center">
-    <h1 class="col-span-2 justify-self-center">
+<section>
+    <img alt="fotka firmy" src="{{ asset('storage/images/firma_main.webp')}}"
+    class="object-cover h-[550px] w-[100%] brightness-[0.35]">
+    <h1 class="text-gray-50 font-extrabold text-3xl absolute inset-0 flex flex-col items-start justify-center text-center px-4 md:max-w-[50%] p-10">
         Kompletné riešenia v oblasti alternatívnych zdrojov energie
     </h1>
-    <div class="justify-self-center justify-items-center">
-        <p>500+</p>
-        <p>inštalovaných tepelných čerpadiel</p>
-    </div>
-    <div class="justify-self-center justify-items-center">
-        <p>100+</p>
-        <p>inštalovaných elektrární</p>
+    <div class="bg-gray-200 h-[80vh] p-2 grid grid-cols-2 items-center">
+        <div class="justify-self-center justify-items-center">
+            <p>500+</p>
+            <p>inštalovaných tepelných čerpadiel</p>
+        </div>
+        <div class="justify-self-center justify-items-center">
+            <p>100+</p>
+            <p>inštalovaných elektrární</p>
+        </div>
     </div>
 </section>
