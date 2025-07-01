df8934ff0bec30aa29d0bb02361511cc48b0954c
MarekDieska <xdieska@stuba.sk>
2025-07-01 16:58:42 +0200

    sluzby

--- IDDesign/resources/views/components/service.blade.php
+++ IDDesign/resources/views/components/service.blade.php
@@ -1,4 +1,43 @@
-<section id="ser">
-    <p>Služby</p>
+<section id="ser" class="bg-gray-800">
+    <p class="text-4xl font-extrabold pt-5 mb-5 text-white">Služby</p>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center">
+        <div class="bg-gray-700 rounded-lg shadow-lg p-3 m-3 max-w-sm border border-gray-700">
+            <img src="{{asset("storage/images/tepelko_priklad-1.webp")}}" alt="fotka tepelného čerpadla" class="rounded-t-lg w-full h-48 object-cover mb-4">
+            <h2 class="text-2xl font-bold mb-2 text-white">Tepelné čerpadlá</h2>
+            <p class="text-white mb-4 font-semibold text-l">
+                Tepelné čerpadlo pre rodinný dom je úsporné riešenie, ktoré zabezpečuje príjemnú teplotu v interiéri počas celého roka. Využíva energiu z okolitého prostredia, čím znižuje náklady na vykurovanie aj chladenie. Okrem úspory nákladov patrí medzi výhody aj ekologická prevádzka a nízka údržba.
+            </p>
+        </div>
+        <div class="bg-gray-700 rounded-lg shadow-lg p-3 m-3 max-w-sm border border-gray-700">
+            <img src="{{asset("storage/images/ac_service.webp")}}" alt="fotka tepelného čerpadla" class="rounded-t-lg w-full h-48 object-cover mb-4">
+            <h2 class="text-2xl font-bold mb-2 text-white">Klimatizácie</h2>
+            <p class="text-white mb-4 font-semibold text-l">
+                Moderné klimatizačné jednotky sú nie len vysoko úsporné ale taktiež dokážu efektívne čistiť vzduch v domácnosti. Klimatizácia dokáže veľmi rýchlo schladiť miestnosť na príjemnú teplotu a taktiež zaručí príjemnejší spánok. Klimatizáciu je taktiež možné ovládať pomocou smartfónu alebo ovládaču.
+            </p>
+        </div>
+        <div class="bg-gray-700 rounded-lg shadow-lg p-3 m-3 max-w-sm border border-gray-700">
+            <img src="{{asset("storage/images/nabijacia_stanica.webp")}}" alt="fotka tepelného čerpadla" class="rounded-t-lg w-full h-48 object-cover mb-4">
+            <h2 class="text-2xl font-bold mb-2 text-white">Nabíjacie stanice</h2>
+            <p class="text-white mb-4 font-semibold text-l">
+                Montáž nabíjacích staníc pre elektromobily priamo u vás doma prináša pohodlie, úsporu času a nižšie prevádzkové náklady. Domáce nabíjanie je praktické riešenie, ktoré zjednodušuje každodenné používanie elektromobilu. Poskytujeme kompletné služby od návrhu a inštalácie až po údržbu, aby bolo nabíjanie spoľahlivé a bezstarostné. Elektromobilita zároveň ponúka tichú a ekologickú alternatívu k tradičnej doprave.
+            </p>
+        </div>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 justify-items-center">
+        <div class="bg-gray-700 rounded-lg shadow-lg p-3 m-3 max-w-sm border border-gray-700">
+            <img src="{{asset("storage/images/battery_service.webp")}}" alt="fotka tepelného čerpadla" class="rounded-t-lg w-full h-48 object-cover mb-4">
+            <h2 class="text-2xl font-bold mb-2 text-white">Fotovoltika</h2>
+            <p class="text-white mb-4 font-semibold text-l">
+                Špecializujeme sa na inštalácie s batériami s funkciou záložného zdroja. Fotovoltikou sa zaoberáme viac ako 15 rokov, k nášmu vybaveniu patria zariadenia na meranie účinnosti fotovoltických meničov, analyzátory siete a merací prístroj pre zmeranie výkonu a charakteristiky fotovoltických panelov.
+            </p>
+        </div>
+        <div class="bg-gray-700 rounded-lg shadow-lg p-3 m-3 max-w-sm border border-gray-700">
+            <img src="{{asset("storage/images/dodavka_zboku.jpg")}}" alt="fotka tepelného čerpadla" class="rounded-t-lg w-full h-48 object-cover mb-4">
+            <h2 class="text-2xl font-bold mb-2 text-white">Servis</h2>
+            <p class="text-white mb-4 font-semibold text-l">
+                Vieme vykonať diagnostiku elektrickej časti tepelného čerpadla, diagnostiku elektrickej siete miesta pripojenia ako aj elektrickú revíziu. Sme vybavení špičkovou meracou technikou. Poskytujeme nielen záručný ale aj pozáručný servis na všetky časti tepelného čerpadla. Sme registrovaní v Slovenskom zväze pre chladiarensku techniku a sme oprávnení nakupovať a plniť chladiace zmesi pre tepelné čerpadlá.
+            </p>
+        </div>
+    </div>
     <br><br>
 </section>
