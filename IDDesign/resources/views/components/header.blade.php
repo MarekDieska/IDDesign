78b21db6e560b89f7579e30fe8889e8671a409fc
MarekDieska <xdieska@stuba.sk>
2025-07-01 17:29:15 +0200

    header tlacidla, favicon, supply start

--- IDDesign/resources/views/components/header.blade.php
+++ IDDesign/resources/views/components/header.blade.php
@@ -1,11 +1,11 @@
 <header class="grid grid-cols-12 bg-black items-center">
     <img
-        class="w-28 col-span-8 sm:justify-center md:justify-start ms-5"
+        class="w-28 col-span-6 md:col-span-8 ms-10"
         src="{{ asset('storage/images/logo.webp') }}"
         alt="logo firmy">
-    <nav class="grid grid-cols-1 sm:grid-rows-2 md:grid-cols-2 md:grid-rows-1 items-center gap-1 text-white col-span-4 me-5">
-        <a href="#for" class="justify-self-end font-bold">Formulár</a>
-        <a href="#con" class="justify-self-end font-bold">Kontakt</a>
+    <nav class="grid grid-cols-1 sm:grid-rows-2 md:grid-cols-2 md:grid-rows-1 items-center gap-1 text-white col-span-6 md:col-span-4">
+        <a href="#for" class="justify-self-end font-bold border rounded-3xl m-2 py-2 px-5">Formulár</a>
+        <a href="#con" class="justify-self-end md:justify-self-start font-bold bg-white text-black rounded-3xl m-2 py-2 px-5">Kontakt</a>
     </nav>
 </header>
 
--- IDDesign/resources/views/components/supply.blade.php
+++ IDDesign/resources/views/components/supply.blade.php
@@ -1,4 +1,4 @@
-<section>
-    <p>Dodávatelia</p>
+<section class="bg-gray-800">
+    <h2 class="text-4xl p-5 mb-10 font-extrabold text-white">Dodávatelia</h2>
     <br><br>
 </section>
